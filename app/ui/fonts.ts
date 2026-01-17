c7876da465ca413c8fd7e6b1c01497add94023dd
ArnobD3v <arnobstack0x@gmail.com>
2026-01-17 19:49:28 +0600

    Clean build ready for Vercel deployment

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,7 +4,7 @@ import { Archivo,Oswald, Lusitana} from "next/font/google";
 export const archivo = Archivo({ subsets: ["latin"] });
 export const oswald = Oswald({ subsets: ["latin"] });
 // Secondary font
-export const lusitana = Lusitana({
+export const lusitana  = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
